9972b7b70c22476d2deca21eb07c42f32c0d3923
idudal <164248554+idudal@users.noreply.github.com>
2024-04-10 15:53:10 +0200

    test

--- py_strings.py
+++ py_strings.py
@@ -1,69 +1,33 @@
 def reverse(text: str) -> str:
-    """
-    Return the 'text' backwards.
+   
+   return text[::-1]
 
-    Parameters
-    ----------
-    text: str
-        The input string
 
-    Returns
-    -------
-    str
-        The text written backwards.
-    """
-    pass
 
 
 def first_to_upper(text: str) -> str:
-    """
-    Changes each first character of the word to uppercase.
-
-    Parameters
-    ----------
-    text: str
-        The input string
-
-    Returns
-    -------
-    str
-        The modified text
-    """
-    pass
+ 
+    words = text.split()
+    capitalized_words = [word[0].upper() + word[1:] for word in words]
+    return ' '.join(capitalized_words)
 
 
 def count_vowels(text: str) -> int:
-    """
-    Counts number of vovels in the text.
-
-    Parameters
-    ----------
-    text: str
-        The input string
-
-    Returns
-    -------
-    inp
-        Number of vowels.
-    """
-    pass
+    vowels = "aeiouAEIOUyY"
+    count = 0
+    for char in text:
+        if char in vowels:
+            count += 1
+    return count
 
 
 def sum_digits(text: str) -> int:
-    """
-    Finds all digitts in the text and returns its sum.
-
-    Parameters
-    ----------
-    text: str
-        The input string
-
-    Returns
-    -------
-    int
-        Sum of all digits in the text.
-    """
-    pass
+  digits = "0123456789"
+  sum = 0
+  for char in text:
+      if char in digits:
+            sum += int(char)
+  return sum
 
 
 def search_substr(text: str, sub: str) -> int:
