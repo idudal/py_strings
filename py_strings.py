a6a96faf321979349d5e9aa78032ae1578ceb5b7
idudal <Dominik.Majewski@studentuj.edu.pl>
2024-04-11 11:12:19 +0200

    q

--- py_strings.py
+++ py_strings.py
@@ -1,50 +1,17 @@
 # pylint:
 def reverse(text: str) -> str:   
-"""
-    Return the 'text' backwards.
-    Parameters
-    ----------
-    text: str
-        The input string
-    Returns
-    -------
-    str
-        The text written backwards.
-    """
     return text[::-1]
 
 
 
 
 def first_to_upper(text: str) -> str:
-    """
-    Changes each first character of the word to uppercase.
-    Parameters
-    ----------
-    text: str
-        The input string
-    Returns
-    -------
-    str
-        The modified text
-    """
     words = text.split()
     capitalized_words = [word[0].upper() + word[1:] for word in words]
     return ' '.join(capitalized_words)
 
 
 def count_vowels(text: str) -> int:
-    """
-    Counts number of vovels in the text.
-    Parameters
-    ----------
-    text: str
-        The input string
-    Returns
-    -------
-    inp
-        Number of vowels.
-    """
     vowels = "aeiouAEIOUyYęąóĘĄÓ"
     count = 0
     for char in text:
@@ -54,17 +21,6 @@ def count_vowels(text: str) -> int:
 
 
 def sum_digits(text: str) -> int:
-    """
-    Finds all digitts in the text and returns its sum.
-    Parameters
-    ----------
-    text: str
-        The input string
-    Returns
-    -------
-    int
-        Sum of all digits in the text.
-    """
     digits = "0123456789"
     suma = 0
     for char in text:
